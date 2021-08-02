cd5411964ba54f36406146680924c43b68504cd6
zp <1336134195@qq.com>
2021-08-02 21:41:04 +0800

    数组

---
+++ src/main/java/com/athome/DataStructuresAndAlgorithms/arrayDemo.java
@@ -0,0 +1,12 @@
+package com.athome.DataStructuresAndAlgorithms;
+
+/**
+ * @Author:
+ * @Date: 2021/8/2 21:22
+ * @Description: 数组特点：1.因为连续性，导致删除和插入只能通过移位来完成，因此插入和删除效率低下
+ * 2.存放相同类型数据
+ * 3.随机访问
+ */
+public class arrayDemo {
+
+}
